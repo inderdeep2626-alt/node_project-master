038e331e0219ed067183111e70472f3ae675d594
inderdeep26 <inderdeep2626@gmail.com>
2026-04-13 15:02:51 -0400

    update

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -21,7 +21,31 @@ async function generateToken(req, res) {
 
 
 }
-module.exports = {generateToken};
+async function verifyToken(req, res) {
+    const secret_key = "super_secret_key";
+    //extract the token from the request header
+        //some_text auth_token
+        //['some_text', 'auth_token']
+        //['auth_token']
+        const token = req.headers.authorization.split(' ')[1];
+
+        if (!token) res.status(401).json({ message: 'You are not Unauthorized' });
+
+    try {
+        const decoded = jwt.verify(token, secret_key);
+
+        if (decoded) {
+            res.json({ message: 'Token is valid', decoded });
+        } else{
+            res.status(401).json({ message: 'Invalid token' });
+        } 
+        
+         } catch (error) {
+        res.status(401).json({ message: 'Internal Server Error',  });
+                }
+                
+}
+module.exports = {generateToken, verifyToken};
 
 
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require('../controllers/authControllers.js');
 
 router.get('/generate-token', authControllers.generateToken);
+router.get('/verify-token', authControllers.verifyToken);
 
 
 
